c4e54fb5a00d7eda97b7b2726c4229a295b3732f
horikawa <nesheep.dream@gmail.com>
2022-10-13 09:02:50 +0900

    init lambda

--- backend/api/cmd/lambda/main.go
+++ backend/api/cmd/lambda/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"doodemo-cook/api/database"
+	"doodemo-cook/api/server"
+	"log"
+	"os"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
+)
 
 func main() {
-	fmt.Println("doodemo cook!")
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("you must set your 'MONGODB_URI' environmental variable")
+	}
+
+	db, disconnect, err := database.New(context.Background(), uri)
+	if err != nil {
+		log.Fatalf("failed to connect to db: %v", err)
+	}
+	defer func() {
+		if err := disconnect(context.Background()); err != nil {
+			log.Printf("failed to disconnect db: %v", err)
+		}
+	}()
+
+	r := server.NewRouter(db)
+
+	lambda.Start(httpadapter.New(r).ProxyWithContext)
 }
